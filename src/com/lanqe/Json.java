67be5dbe1809552aaba26a85045e4ff75cb4ce53
lanqinger <37525061+lanqinger@users.noreply.github.com>
2018-03-21 10:19:25 +0800

    transformJson

--- src/com/lanqe/Json.java
+++ src/com/lanqe/Json.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 public class Json {
@@ -24,10 +25,31 @@ public class Json {
 		return map;
 	}
 	
+	public static JSONObject transformJson(Map<String, Object> map) {
+		return JSONObject.fromObject(map);
+	}
+	
+	public static JSONArray transformJsonArray(List<String> list) {
+		return JSONArray.fromObject(list);
+	}
+	
 	public static void main(String[] args) {
 		String result = "{\"isSuccess\":\"true\",\"list\":[{\"name\":\"林叶\",\"age\":\"21\"},{\"name\":\"叶萌\",\"age\":\"20\"}]}";
 		Map<String,Object> map = getJson(result);
 		System.out.println(map);
+		
+		Map<String, Object> map2 = new HashMap<String, Object>();
+		map2.put("success","1");
+		map2.put("message", "执行成功！");
+		JSONObject json = transformJson(map2);
+		System.out.println(json);
+		
+		List<String> list = new ArrayList<String>();
+		list.add("杭州市");
+		list.add("温州市");
+		list.add("丽水市");
+		JSONArray array = transformJsonArray(list);
+		System.out.println(array);
 	}
 
 }
